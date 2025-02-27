411772eade0f530f176c76f2da60a229c00af197
SUPRIYA K <114737299+SUPRIYAKUKUNURU@users.noreply.github.com>
2025-02-27 20:15:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Chatbot from './Chatbot';
 
 function App() {
   return (
     <div className="App">
-     <h1>Chatbot</h1>
+      <h1>Chatbot</h1>
+   <Chatbot />
     </div>
   );
 }
